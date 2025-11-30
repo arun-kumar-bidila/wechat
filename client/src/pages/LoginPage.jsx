eb01905ce5b1d502af2a5c50fdde33d5252964d9
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2025-11-30 14:00:57 +0530

    login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,129 @@
-import React from 'react'
-
+import React, { use, useState } from "react";
+import assets from "../assets/assets.js";
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+  const [currState, setCurrState] = useState("Sign Up");
+  const [fullName, setfullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [bio, setBio] = useState("");
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
-export default LoginPage
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (currState === "Sign Up" && !isDataSubmitted) {
+      setIsDataSubmitted(true);
+    }
+    return;
+  };
+
+  return (
+    <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
+      {/*left section image*/}
+      <img src={assets.logo_big} alt="" className="w-[min(30vw,250px)]" />
+
+      {/*right section form*/}
+      <form
+        onSubmit={onSubmitHandler}
+        className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg"
+      >
+        <h2 className="font-medium text-2xl flex justify-between items-center">
+          {currState}
+          {isDataSubmitted && (
+            <img
+              className="w-5 cursor-pointer"
+              onClick={() => setIsDataSubmitted(false)}
+              src={assets.arrow_icon}
+              alt=""
+            />
+          )}
+        </h2>
+        {currState === "Sign Up" && !isDataSubmitted && (
+          <input
+            onChange={(e) => setfullName(e.target.value)}
+            value={fullName}
+            type="text"
+            placeholder="Full Name"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none"
+            required
+          />
+        )}
+        {!isDataSubmitted && (
+          <>
+            <input
+              type="email"
+              placeholder="email address"
+              required
+              className="p-2
+             border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              className="p-2
+             border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </>
+        )}
+        {currState === "Sign Up" && isDataSubmitted && (
+          <textarea
+            rows={4}
+            placeholder="provide a short bio"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            required
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+        )}
+
+        <button
+          type="submit"
+          className="py-3 bg-linear-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer"
+        >
+          {currState === "Sign Up" ? "Create Account" : "Login Now"}
+        </button>
+
+        <div className="flex items-center gap-2 text-sm text-gray-50">
+          <input type="checkbox" />
+          <p>Agree to the terms of use & privacy Policy</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          {currState === "Sign Up" ? (
+            <p className="text-sm text-gray-600">
+              Already have an account
+              <span
+                onClick={() => {
+                  setCurrState("Login");
+                  setIsDataSubmitted(false);
+                }}
+                className="font-medium text-violet-500 cursor-pointer px-2"
+              >
+                {" "}
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className="text-sm text-gray-600 ">
+              Create an account
+              <span
+                onClick={() => {
+                  setCurrState("Sign Up");
+                }}
+                className="font-medium text-violet-500 cursor-pointer px-2"
+              >
+                Click Here
+              </span>
+            </p>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
